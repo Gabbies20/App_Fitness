ef777a4cb389d28c6e8be1e1f7b4a2228371565c
Gabbies20 <gabriela.pinzon.ochoa.al@iespoligonosur.org>
2023-11-05 23:05:20 +0100

    Creada URL 2

--- fitness/urls.py
+++ fitness/urls.py
@@ -4,4 +4,6 @@ from .import views
 urlpatterns = [
     path('',views.index,name='index'),
     path('ejercicios/listar',views.lista_ejercicios,name='lista_ejercicios'),
+    path('informacion_usuario/',views.informacion_usuario,name='informacion_usuario'),
+    path('perfil_usuario/<int:id_usuario>/',views.perfil_usuario,name='perfil_usuario')
 ]
--- fitness/views.py
+++ fitness/views.py
@@ -1,10 +1,22 @@
-from django.shortcuts import render
-from .models import Ejercicio
+from django.shortcuts import render, get_object_or_404
+from .models import Ejercicio,Usuario,Perfil_de_Usuario
 
 # Create your views here.
 def index(request):
     return render (request,'fitness/index.html',{})
 
+
+
+#1.Vista que muestra todos los ejercicios y sus datos, incluidos los relcionados.
 def lista_ejercicios(request):
     ejercicios = Ejercicio.objects.prefetch_related('usuarios')
     return render(request,'fitness/lista_ejercicios.html',{'ejercicios_mostrar':ejercicios})
+
+#2.Vista que muestra la información de un usuario especifico, incluyendo su nombre, correo y enlace a su perfil de usuario.
+def informacion_usuario(request):
+    usuario = Usuario.objects.all()
+    return render(request,'fitness/informacion_usuario.html',{'usuario_mostrar':usuario})
+
+def perfil_usuario(request, id_usuario):
+    perfil = Perfil_de_Usuario.objects.filter(usuario_id=id_usuario).first()
+    return render(request, 'fitness/perfil_usuario.html', {'perfil_mostrar': perfil})
