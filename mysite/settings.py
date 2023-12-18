48fc2b455e9ccfe83407095b2b1cedace578a724
Gabbies20 <gabriela.pinzon.ochoa.al@iespoligonosur.org>
2023-12-18 17:05:04 +0100

    Sigue error con el debug_toolbar

--- mysite/settings.py
+++ mysite/settings.py
@@ -41,7 +41,14 @@ INSTALLED_APPS = [
     'django_seed',
     'bootstrap5',
     'django_bootstrap_icons',
+    'crispy_forms',
+    'crispy_bootstrap5',
 ]
+
+
+CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
+
+CRISPY_TEMPLATE_PACK = "bootstrap5"
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
