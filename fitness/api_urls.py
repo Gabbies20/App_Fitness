361d88f195edd4273a6b4ad88dc9f3124f0956ac
Gabbies20 <gabriela.pinzon.ochoa.al@iespoligonosur.org>
2024-01-31 02:31:59 +0000

    Hecho desde PW

--- fitness/api_urls.py
+++ fitness/api_urls.py
@@ -4,10 +4,5 @@ from .api_views import *
 urlpatterns = [
     path('ejercicios',ejercicio_list),
     path('ejercicios/busqueda_simple',ejercicio_buscar),
-<<<<<<< HEAD
-   # path('ejercicios/busqueda_avanzada',ejercici)
 ]
-=======
-    #path('ejercicios/busqueda_avanzada',ejercici)
-]
->>>>>>> 02f75532ba09a82c7b69fa3e4382ec774643e968
+
